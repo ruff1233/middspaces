76ac08a6b59cca30ff9dee07408e946a21406a90
Paul Ruffolo <ruff1233@gmail.com>
2022-11-29 00:13:28 -0500

    Added log file, reservations are now loaded by date and space and disables all dropdown time options for the chosen date

--- src/pages/NewReservation.js
+++ src/pages/NewReservation.js
@@ -14,16 +14,21 @@ import TextField from '@mui/material/TextField';
 import { resRef, spaceRef } from '../firebase';
 import { child, get } from "firebase/database";
 import { useParams } from 'react-router-dom';
+import { SettingsBackupRestoreSharp } from "@mui/icons-material";
 
 function NewReservation(props) {
-    const [date, setDate] = React.useState(new Date());
+    const [date, setDate] = React.useState("");
     const [time, setTime] = React.useState("");
     const [space, setSpace] = React.useState({});
     const [open, setOpen] = React.useState(false);
+    const [reservations, setReservations] = React.useState([]);
+    const [bookings, setBookings] = React.useState(new Map());
+    const [rerender, setRerender] = React.useState(false);
 
     let { spaceName } = useParams();
 
     React.useEffect(() => {
+        //Load in space information
         get(child(spaceRef, `${spaceName}`)).then((snapshot) => {
           if (snapshot.exists()) {
             setSpace(snapshot.val());
@@ -35,12 +40,28 @@ function NewReservation(props) {
         });
       }, []);
 
-      const handleClick = () => {
+    const handleClick = () => {
         setOpen(true);
-      };
+    };
 
     const handleDateChange = (date:Date) => {
-        setDate(date)
+        bookings.clear();
+        var styleDate = (date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear();
+        //Load in reservations for selected space on selected date
+        get(resRef.orderByChild("date").equalTo(styleDate)).then((snapshot) => {
+            if (snapshot.exists()) {
+                snapshot.forEach(function(resSnapshot) {
+                    setBookings(bookings.set(resSnapshot.val().time, false));
+                    console.log(resSnapshot.val());
+                });
+            } else {
+                console.log("No data available");
+            }
+        }).catch((error) => {
+            console.error(error);
+      });
+      setTime('');
+      setDate(date);
     }
 
     const handleTimeChange = (event: SelectChangeEvent) => {
@@ -50,6 +71,10 @@ function NewReservation(props) {
     const CustomPicker = React.forwardRef(({ value, onClick }, ref) => (
         <TextField size="small" label="Date" onClick={onClick} value={value} ref={ref} />
     ));
+
+    function useForceUpdate(){
+        setRerender(!rerender);
+    }
 
     return (
     <div>
@@ -67,33 +92,33 @@ function NewReservation(props) {
                 <DatePicker selected={date} onChange={(e) => handleDateChange(e)} customInput={<CustomPicker />} />
             </Box>
             <Box sx={{ flexDirection: 'row' }} style={{padding: 15, textAlign:"center"}}>
-                <FormControl size="small" style={{width: 210}} >
+                <FormControl disabled={!date} size="small" style={{width: 210}} >
                     <InputLabel>Time</InputLabel>
-                    <Select value={time} label="Time" onChange={handleTimeChange}>
-                        <MenuItem value={"8:00am-8:30am"}>8:00am-8:30am</MenuItem>
-                        <MenuItem value={"8:30am-9:00am"}>8:30am-9:00am</MenuItem>
-                        <MenuItem value={"9:00am-9:30am"}>9:00am-9:30am</MenuItem>
-                        <MenuItem value={"9:30am-10:00am"}>9:30am-10:00am</MenuItem>
-                        <MenuItem value={"10:00am-10:30am"}>10:00am-10:30am</MenuItem>
-                        <MenuItem value={"10:30am-11:00am"}>10:30am-11:00am</MenuItem>
-                        <MenuItem value={"11:00am-11:30am"}>11:00am-11:30am</MenuItem>
-                        <MenuItem value={"11:30am-12:00pm"}>11:30am-12:00pm</MenuItem>
-                        <MenuItem value={"12:00pm-12:30pm"}>12:00pm-12:30pm</MenuItem>
-                        <MenuItem value={"12:30pm-1:00pm"}>12:30pm-1:00pm</MenuItem>
-                        <MenuItem value={"1:00pm-1:30pm"}>1:00pm-1:30pm</MenuItem>
-                        <MenuItem value={"1:30pm-2:00pm"}>1:30pm-2:00pm</MenuItem>
-                        <MenuItem value={"2:00pm-2:30pm"}>2:00pm-2:30pm</MenuItem>
-                        <MenuItem value={"2:30pm-3:00pm"}>2:30pm-3:00pm</MenuItem>
-                        <MenuItem value={"3:00pm-3:30pm"}>3:00pm-3:30pm</MenuItem>
-                        <MenuItem value={"3:30pm-4:00pm"}>3:30pm-4:00pm</MenuItem>
-                        <MenuItem value={"4:00pm-4:30pm"}>4:00pm-4:30pm</MenuItem>
-                        <MenuItem value={"4:30pm-5:00pm"}>4:30pm-5:00pm</MenuItem>
-                        <MenuItem value={"5:00pm-5:30pm"}>5:00pm-5:30pm</MenuItem>
-                        <MenuItem value={"5:30pm-6:00pm"}>5:30pm-6:00pm</MenuItem>
-                        <MenuItem value={"6:00pm-6:30pm"}>6:00pm-6:30pm</MenuItem>
-                        <MenuItem value={"6:30pm-7:00pm"}>6:30pm-7:00pm</MenuItem>
-                        <MenuItem value={"7:00pm-7:30pm"}>7:00pm-7:30pm</MenuItem>
-                        <MenuItem value={"7:30pm-8:00pm"}>7:30pm-8:00pm</MenuItem>
+                    <Select onOpen={useForceUpdate} value={time} label="Time" onChange={handleTimeChange}>
+                        <MenuItem disabled={bookings.has("8:00am-8:30am")} value={"8:00am-8:30am"}>8:00am-8:30am</MenuItem>
+                        <MenuItem disabled={bookings.has("8:30am-9:00am")} value={"8:30am-9:00am"}>8:30am-9:00am</MenuItem>
+                        <MenuItem disabled={bookings.has("9:00am-9:30am")} value={"9:00am-9:30am"}>9:00am-9:30am</MenuItem>
+                        <MenuItem disabled={bookings.has("9:30am-10:00am")} value={"9:30am-10:00am"}>9:30am-10:00am</MenuItem>
+                        <MenuItem disabled={bookings.has("10:00am-10:30am")} value={"10:00am-10:30am"}>10:00am-10:30am</MenuItem>
+                        <MenuItem disabled={bookings.has("10:30am-11:00am")} value={"10:30am-11:00am"}>10:30am-11:00am</MenuItem>
+                        <MenuItem disabled={bookings.has("11:00am-11:30am")} value={"11:00am-11:30am"}>11:00am-11:30am</MenuItem>
+                        <MenuItem disabled={bookings.has("11:30am-12:00pm")} value={"11:30am-12:00pm"}>11:30am-12:00pm</MenuItem>
+                        <MenuItem disabled={bookings.has("12:00pm-12:30pm")} value={"12:00pm-12:30pm"}>12:00pm-12:30pm</MenuItem>
+                        <MenuItem disabled={bookings.has("12:30pm-1:00pm")} value={"12:30pm-1:00pm"}>12:30pm-1:00pm</MenuItem>
+                        <MenuItem disabled={bookings.has("1:00pm-1:30pm")} value={"1:00pm-1:30pm"}>1:00pm-1:30pm</MenuItem>
+                        <MenuItem disabled={bookings.has("1:00pm-1:30pm")} value={"1:00pm-1:30pm"} value={"1:30pm-2:00pm"}>1:30pm-2:00pm</MenuItem>
+                        <MenuItem disabled={bookings.has("2:00pm-2:30pm")} value={"2:00pm-2:30pm"}>2:00pm-2:30pm</MenuItem>
+                        <MenuItem disabled={bookings.has("2:30pm-3:00pm")} value={"2:30pm-3:00pm"}>2:30pm-3:00pm</MenuItem>
+                        <MenuItem disabled={bookings.has("3:00pm-3:30pm")} value={"3:00pm-3:30pm"}>3:00pm-3:30pm</MenuItem>
+                        <MenuItem disabled={bookings.has("3:30pm-4:00pm")} value={"3:30pm-4:00pm"}>3:30pm-4:00pm</MenuItem>
+                        <MenuItem disabled={bookings.has("4:00pm-4:30pm")} value={"4:00pm-4:30pm"}>4:00pm-4:30pm</MenuItem>
+                        <MenuItem disabled={bookings.has("4:30pm-5:00pm")} value={"4:30pm-5:00pm"}>4:30pm-5:00pm</MenuItem>
+                        <MenuItem disabled={bookings.has("5:00pm-5:30pm")} value={"5:00pm-5:30pm"}>5:00pm-5:30pm</MenuItem>
+                        <MenuItem disabled={bookings.has("5:30pm-6:00pm")} value={"5:30pm-6:00pm"}>5:30pm-6:00pm</MenuItem>
+                        <MenuItem disabled={bookings.has("6:00pm-6:30pm")} value={"6:00pm-6:30pm"}>6:00pm-6:30pm</MenuItem>
+                        <MenuItem disabled={bookings.has("6:30pm-7:00pm")} value={"6:30pm-7:00pm"}>6:30pm-7:00pm</MenuItem>
+                        <MenuItem disabled={bookings.has("7:00pm-7:30pm")} value={"7:00pm-7:30pm"}>7:00pm-7:30pm</MenuItem>
+                        <MenuItem disabled={bookings.has("7:30pm-8:00pm")} value={"7:30pm-8:00pm"}>7:30pm-8:00pm</MenuItem>
                     </Select>
                 </FormControl>
             </Box>
