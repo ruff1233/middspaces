50f3f2f2bc42093a02675e68cdbe3e5f6fcfc40d
EmmaDoubman <edoubman@middlebury.edu>
2022-12-06 17:16:52 -0500

    Took edit button off the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,15 +13,18 @@ function Home(props) {
         <Typography align="center" variant="h4" component="div" sx={{ flexGrow: 1 }}>
           Welcome to MiddSpaces!
         </Typography>
-        <Stack spacing={4} direction="column" alignItems="center">
+        <Typography align="center" variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Pick the floor you want to reserve a space on:
+        </Typography>
+        <Stack spacing={4} direction="column" alignItems="center" justifyContent="center">
         {/* <Button variant="contained" onClick={() => {navigate('/space')}}>Example Space</Button>
         <Button variant="contained" onClick={() => {navigate('/newres/testSpace')}}>Example New Reservation</Button> 
-        <Button variant="contained" onClick={() => {navigate('/editres/testRes2')}}>Example Edit Reservation</Button>*/}
+        <Button variant="contained" onClick={() => {navigate('/editres/testRes2')}}>Example Edit Reservation</Button>
+        <Button variant="contained" onClick={() => {navigate('/editres/testRes2')}}>Edit Reservation</Button>*/}
         <Button variant="contained" onClick={() => {navigate('/floor3')}}>Floor 3</Button>
         <Button variant="contained" onClick={() => {navigate('/floor4')}}>Floor 4</Button>
         <Button variant="contained" onClick={() => {navigate('/floor5')}}>Floor 5</Button>
         <Button variant="contained" onClick={() => {navigate('/floor6')}}>Floor 6</Button>
-        <Button variant="contained" onClick={() => {navigate('/editres/testRes2')}}>Edit Reservation</Button>
         </Stack>
     </div>
     );
